5577003a1301b74df0e3928a5711a5367292f0bb
Gil Hilário <gil@civity.nl>
2023-04-25 14:40:53 +0200

    WIP

--- ckanext/scorewater/plugin.py
+++ ckanext/scorewater/plugin.py
@@ -2,6 +2,7 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import validators as v
 
+
 class ScorewaterPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IValidators)
@@ -9,11 +10,9 @@ class ScorewaterPlugin(plugins.SingletonPlugin):
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
-        toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic', 'scorewater')
 
     # IValidator
     def get_validators(self):
         return {
-                'url_checker': v.url_checker
-                }
+            'url_checker': v.url_checker
+        }
